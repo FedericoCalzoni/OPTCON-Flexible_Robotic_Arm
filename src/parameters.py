0348e7a5dc09e0585e5cb80dfdcdff5233085f35
MicheleBosi <michele.bosi99@gmail.com>
2024-12-12 16:42:11 +0100

    Aggiunte le matrici di costo come parametri.

--- src/parameters.py
+++ src/parameters.py
@@ -1,3 +1,4 @@
+import numpy as np
 # Dynamics parameters
 M1 = 2
 M2 = 2
@@ -11,7 +12,12 @@ G = 9.81
 F1 = 0.1
 F2 = 0.1
 
-# visualizer parameters
+# Visualizer parameters
 t_i = 0
 t_f = 10
 dt = 1e-4
+
+# Cost Function parameters
+Qt = np.diag([1, 1, 1, 1])
+Rt = np.diag([1, 0, 0, 0])
+QT = np.diag([10000, 10000, 10000, 10000])
