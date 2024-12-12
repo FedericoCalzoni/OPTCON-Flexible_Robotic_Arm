657a0c14c089a12c2d4a35df0e89fad42daade42
MicheleBosi <michele.bosi99@gmail.com>
2024-12-12 16:43:03 +0100

    Aggiunto uno script per debuggare il calcolo della funzione di costo. Rimuovere se necessario.

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import numpy as np
 from parameters import L1, L2, t_i, t_f, dt
 from newton_method import newton_method
 from matplotlib import pyplot as plt
+import Cost
 
 def main():
     """
@@ -30,6 +31,8 @@ def main():
     
     x_0 = np.array([[0], [0], [eq1_0], [eq1_1]])
     u_0 = np.array([[eq1_2], [0], [0], [0]])
+   
+
 
     # Compute dynamics for each time step
     time_intervals = int((t_f - t_i) / dt + 1)
@@ -44,6 +47,40 @@ def main():
     
     # plt.plot(x_e)
     
+    
+    ###### TASK 1 - J computation trials - Mike
+    x_f = np.array([[0], [0], [eq2_0], [eq2_1]])
+    u_f = np.array([[eq2_2], [0], [0], [0]])
+
+    x_ref = np.zeros([4, time_intervals])
+    u_ref = np.zeros([4, time_intervals])
+
+    # Build the reference curve which must be used to compute the cost function
+    for i in range (int(time_intervals / 2)):
+        x_ref[:, i] = x_0.flatten()
+        u_ref[:, i] = u_0.flatten()
+
+    for i in range (int(time_intervals/2) , time_intervals):
+        x_ref[:, i] = x_f.flatten()
+        u_ref[:, i] = u_f.flatten()
+
+    # Build the x and u trajectory which permanently stays on the first equilibrium 
+    # (from t =  0 up to t = T) as an initial guess for the newton method for optimal control
+    # Fede ha già chiamato x_trajectory come x_history. Sono la stessa cosa ma volevo avere
+    # un'istanza differente per poter debuggare senza generare potenziali errori l'un l'altro.
+    x_trajectory = np.zeros([4, time_intervals])
+    u_trajectory = np.zeros([4, time_intervals]) 
+    for i in range (time_intervals):
+        x_trajectory[:, i] = x_0.flatten()
+        u_trajectory[:, i] = u_0.flatten()
+
+    J = Cost.J_Function(x_trajectory, u_trajectory, x_ref, u_ref)
+    print(time_intervals)
+    print(J)
+    #########
+
+
+
     # Convert state history to a matrix
     matrix_x_history = np.hstack(x_history)
 
