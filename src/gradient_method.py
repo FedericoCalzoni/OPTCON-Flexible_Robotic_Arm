30c5497a8870caef063ee8fb14a4c651d910ec1a
FedericoCalzoni <federicocalzoni01@gmail.com>
2025-01-03 18:03:13 +0100

    update gradient_method.py

--- src/gradient_method.py
+++ src/gradient_method.py
@@ -4,9 +4,9 @@ import numpy as np
 import armijo
 
 
-def gradient_method(x_init, u_init, x_reference, u_reference, max_iterations=100):
+def gradient_method(x_init, u_init, x_des, u_des, max_iterations=100, tol=1e-5):
 
-    T = x_reference.shape[1]
+    T = x_des.shape[1]
     
     x_temp = np.zeros((x_init.shape[0], T))
     u_temp = np.zeros((u_init.shape[0], T))
@@ -14,46 +14,55 @@ def gradient_method(x_init, u_init, x_reference, u_reference, max_iterations=100
     x_temp = x_init
     u_temp = u_init
     
-    J = np.zeros(max_iterations)
-    lmbd = np.zeros((x_init.shape[0], T))
+    x_temp[:,0] = x_des[:,0]
+        
+    J_history = np.zeros(max_iterations)
+    lmbd_traj = np.zeros((x_init.shape[0], T))
     
     for k in range(max_iterations):
         
         # Step 1: Compute descent direction
         
-        J[k] = cost.J_Function(x_temp, u_temp, x_reference, u_reference)
+        J_history[k] = cost.J_Function(x_temp, u_temp, x_des, u_des, "LQR")
         
         # costate terminal condition
-        lmbd[:,T-1] = cost.grad_terminal_cost(x_temp[:,T-1], x_reference[:,T-1]) 
+        lmbd_traj[:,T-1] = cost.grad_terminal_cost(x_temp[:,T-1], x_des[:,T-1]) 
         
         
         for t in reversed(range(T-1)):
-            q_t = cost.grad1_J(x_temp[:,t], x_reference[:,t])
-            r_t = cost.grad2_J(u_temp[:,t], u_reference[:,t])
-            _, dfx_ref, dfu_ref = dyn.dynamics(x_reference[:,t], u_reference[:,t])
+            q_t = cost.grad1_J(x_temp[:,t], x_des[:,t])
+            r_t = cost.grad2_J(u_temp[:,t], u_des[:,t])
+            dfx = dyn.jacobian_x_dot_wrt_x(x_temp[:,t], u_temp[:,t])
+            dfu = dyn.jacobian_x_dot_wrt_u(x_temp[:,t])
+                        
+            At = dfx.T # maybe not transpose
+            Bt = dfu            
             
-            At = dfx_ref.T
-            Bt = dfu_ref.T
-            
-            lmbd[:,t]=At.T@lmbd[:,t+1]+q_t # costate update
+            lmbd_traj[:,t]= q_t + At.T @ lmbd_traj[:,t+1] # costate update
             
             # descent direction
-            dJ_temp = Bt.T@lmbd[:,t+1]+r_t # gradient of J wrt u
-            delta_u = - dJ_temp # negative gradient
+            grad_J_wrt_u = r_t + Bt.T @ lmbd_traj[:,t+1]
+            delta_u = - grad_J_wrt_u # negative gradient
             
         # Step 2 & 3: Compute a new input and state trajectory
         
-        step_size = armijo.select_step_size(x_temp[:,0], # are we using the correct initial state?
-                                            u_temp[:,0], 
-                                            x_reference, 
-                                            u_reference,
-                                            J[k])
+        # step_size = armijo.select_step_size(x_temp[:,0], # are we using the correct initial state?
+        #                                     u_temp[:,0], 
+        #                                     x_des, 
+        #                                     u_des,
+        #                                     J[k])
         
-        # step_size = 0.01
+        step_size = 1
         
         for t in range(T-1):
-            u_temp[:,t] = u_temp[:,t] + step_size * delta_u
-            x_temp[:,t+1] = dyn.dynamics(x_temp[:,t][:,None], u_temp[:,t][:,None])[0].flatten()
+            u_temp[:,t] += step_size * delta_u
+            x_temp[:,t+1] = dyn.dynamics(x_temp[:,t], u_temp[:,t])[0]
             
-    return x_temp, u_temp, J, lmbd
+        if k > 0 and abs(J_history[-1] - J_history[-2]) < tol:
+            break
+        
+        print("x_temp: ", x_temp)
+        print("u_temp: ", u_temp)
+            
+    return x_temp, u_temp, J_history, lmbd_traj
             
