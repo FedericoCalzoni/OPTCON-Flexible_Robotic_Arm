5bade0310442f383b7b7ea3e296dac6a09717740
MicheleBosi <michele.bosi99@gmail.com>
2025-01-04 13:03:34 +0100

    Jacobian evaluated around x(t+1) and not about x_dot

--- src/dynamics.py
+++ src/dynamics.py
@@ -1,5 +1,5 @@
 import numpy as np
-from parameters import M1, M2, L1, R1, R2, I1, I2, G, F1, F2
+from parameters import M1, M2, L1, R1, R2, I1, I2, G, F1, F2, dt
 
 F = np.array([[F1, 0],
               [ 0 ,F2]])
@@ -37,6 +37,10 @@ def dynamics(x, u, dt=1e-3):
     theta1 = x[2].item()
     theta2 = x[3].item()
 
+    tau1 = u[0].item()
+    tau2 = u[1].item()
+    #x = np.array([dtheta1, dtheta2, theta1, theta2])
+
     # Compute matrices
     M = compute_inertia_matrix(theta2)
     M_inv = np.linalg.inv(M)
@@ -64,9 +68,14 @@ def dynamics(x, u, dt=1e-3):
         [np.zeros((2, 1))]
     ])
     
-    x_dot = A @ x + B @ u - M_inv_ext @ (C_ext + G_ext)
+    x_rev = x.reshape(-1, 1) 
+    u_rev = u.reshape(-1, 1)
+
+    ct = - M_inv_ext @ (C_ext + G_ext)
+
+    x_dot = A @ x_rev + B @ u_rev + ct
     
-    x_new = x + dt * x_dot
+    x_new = (x_rev + dt * x_dot).flatten()
     
     #dfx = jacobian_x_dot_wrt_x(dtheta1, dtheta2, theta1, theta2, u[0].item())
     #dfu = jacobian_x_dot_wrt_u(theta2)
@@ -75,7 +84,8 @@ def dynamics(x, u, dt=1e-3):
     #Huu = hessian_x_dot_wrt_u()
     #Hxu = hessian_x_dot_wrt_xu(theta2)
         
-    return x_new #, dfx, dfu, Hxx, Huu, Hxu
+    return x_new
+
 
 # TODO: give a better name to this funciton, it is the jacobian of the gravity
 def jacobian(theta1, theta2):
@@ -87,87 +97,232 @@ def jacobian(theta1, theta2):
     JG_23 = 0
     return np.array([[JG_11 , JG_12, JG_13], [JG_21 , JG_22, JG_23]])
 
-def jacobian_x_dot_wrt_x(dtheta1, dtheta2, theta1, theta2, tau1):
-    dfx = np.zeros((4, 4))
-    # Common Subexpressions:
-    tmp0 = pow(R2, 2)
-    tmp1 = M2*tmp0
-    tmp2 = I2 + tmp1
-    tmp3 = F1*tmp2
-    tmp4 = np.sin(theta2)
-    tmp5 = L1*M2
-    tmp6 = R2*tmp5
-    tmp7 = tmp4*tmp6
-    tmp8 = dtheta2*tmp7
-    tmp9 = 2*tmp8
-    tmp10 = np.cos(theta2)
-    tmp11 = tmp10*tmp6
-    tmp12 = tmp11 + tmp2
-    tmp13 = 2*dtheta1
-    tmp14 = tmp13*tmp7
-    tmp15 = pow(L1, 2)
-    tmp16 = M2*tmp15
-    tmp17 = M1*pow(R1, 2)
-    tmp18 = pow(M2, 2)*tmp0
-    tmp19 = tmp15*tmp18
-    tmp20 = I1*I2 + I1*tmp1 + I2*tmp16 + I2*tmp17 + tmp1*tmp17 + tmp19*pow(tmp4, 2)
-    tmp21 = 1.0/tmp20
-    tmp22 = F2*tmp12
-    tmp23 = dtheta2 + tmp13
-    tmp24 = -tmp23*tmp7 - tmp8
-    tmp25 = theta1 + theta2
-    tmp26 = G*np.cos(tmp25)
-    tmp27 = M2*R2
-    tmp28 = tmp26*tmp27
-    tmp29 = G*(M1*R1 + tmp5)
-    tmp30 = tmp28 + tmp29*np.cos(theta1)
-    tmp31 = F2*tmp8
-    tmp32 = G*np.sin(tmp25)
-    tmp33 = L1*pow(dtheta1, 2)
-    tmp34 = tmp32 + tmp33*tmp4
-    tmp35 = L1*tmp18*tmp34*tmp4
-    tmp36 = -dtheta2*tmp11*tmp23 + tmp28
-    tmp37 = tmp10*tmp33 + tmp26
-    tmp38 = -tmp23*tmp8 + tmp27*tmp32 + tmp29*np.sin(theta1)
-    tmp39 = 2*tmp10*tmp19*tmp4/pow(tmp20, 2)
-    tmp40 = I1 + 2*tmp11 + tmp16 + tmp17 + tmp2
-    tmp41 = F2*tmp40
-    tmp42 = tmp27*tmp40
-    # Jacobian Elements:
-    dfx[0, 0] = tmp21*(tmp12*tmp14 + tmp2*tmp9 - tmp3)
-    dfx[0, 1] = tmp21*(-tmp2*tmp24 + tmp22)
-    dfx[0, 2] = tmp21*(tmp12*tmp28 - tmp2*tmp30)
-    dfx[0, 3] = tmp21*(M2*R2*tmp12*tmp37 - tmp2*tmp36 - tmp31 - tmp35) \
-        - tmp39*(-dtheta1*tmp3 + dtheta2*tmp22 + tau1*tmp2 + tmp12*tmp27*tmp34 - tmp2*tmp38)
-    dfx[1, 0] = tmp21*(F1*tmp12 - tmp12*tmp9 - tmp14*tmp40)
-    dfx[1, 1] = tmp21*(tmp12*tmp24 - tmp41)
-    dfx[1, 2] = tmp21*(tmp12*tmp30 - tmp28*tmp40)
-    dfx[1, 3] = tmp21*(-F1*dtheta1*tmp7 + tau1*tmp7 + tmp12*tmp36 + 2*tmp31 \
-        + 2*tmp35 - tmp37*tmp42 - tmp38*tmp7) - tmp39*(F1*dtheta1*tmp12 \
-            - dtheta2*tmp41 - tau1*tmp12 + tmp12*tmp38 - tmp34*tmp42)
-    dfx[2, 0] = 1
-    dfx[3, 1] = 1
-    return dfx
+#def jacobian_x_dot_wrt_x(dtheta1, dtheta2, theta1, theta2, tau1):
+#    dfx = np.zeros((4, 4))
+#    # Common Subexpressions:
+#    tmp0 = pow(R2, 2)
+#    tmp1 = M2*tmp0
+#    tmp2 = I2 + tmp1
+#    tmp3 = F1*tmp2
+#    tmp4 = np.sin(theta2)
+#    tmp5 = L1*M2
+#    tmp6 = R2*tmp5
+#    tmp7 = tmp4*tmp6
+#    tmp8 = dtheta2*tmp7
+#    tmp9 = 2*tmp8
+#    tmp10 = np.cos(theta2)
+#    tmp11 = tmp10*tmp6
+#    tmp12 = tmp11 + tmp2
+#    tmp13 = 2*dtheta1
+#    tmp14 = tmp13*tmp7
+#    tmp15 = pow(L1, 2)
+#    tmp16 = M2*tmp15
+#    tmp17 = M1*pow(R1, 2)
+#    tmp18 = pow(M2, 2)*tmp0
+#    tmp19 = tmp15*tmp18
+#    tmp20 = I1*I2 + I1*tmp1 + I2*tmp16 + I2*tmp17 + tmp1*tmp17 + tmp19*pow(tmp4, 2)
+#    tmp21 = 1.0/tmp20
+#    tmp22 = F2*tmp12
+#    tmp23 = dtheta2 + tmp13
+#    tmp24 = -tmp23*tmp7 - tmp8
+#    tmp25 = theta1 + theta2
+#    tmp26 = G*np.cos(tmp25)
+#    tmp27 = M2*R2
+#    tmp28 = tmp26*tmp27
+#    tmp29 = G*(M1*R1 + tmp5)
+#    tmp30 = tmp28 + tmp29*np.cos(theta1)
+#    tmp31 = F2*tmp8
+#    tmp32 = G*np.sin(tmp25)
+#    tmp33 = L1*pow(dtheta1, 2)
+#    tmp34 = tmp32 + tmp33*tmp4
+#    tmp35 = L1*tmp18*tmp34*tmp4
+#    tmp36 = -dtheta2*tmp11*tmp23 + tmp28
+#    tmp37 = tmp10*tmp33 + tmp26
+#    tmp38 = -tmp23*tmp8 + tmp27*tmp32 + tmp29*np.sin(theta1)
+#    tmp39 = 2*tmp10*tmp19*tmp4/pow(tmp20, 2)
+#    tmp40 = I1 + 2*tmp11 + tmp16 + tmp17 + tmp2
+#    tmp41 = F2*tmp40
+#    tmp42 = tmp27*tmp40
+#    # Jacobian Elements:
+#    dfx[0, 0] = tmp21*(tmp12*tmp14 + tmp2*tmp9 - tmp3)
+#    dfx[0, 1] = tmp21*(-tmp2*tmp24 + tmp22)
+#    dfx[0, 2] = tmp21*(tmp12*tmp28 - tmp2*tmp30)
+#    dfx[0, 3] = tmp21*(M2*R2*tmp12*tmp37 - tmp2*tmp36 - tmp31 - tmp35) \
+#        - tmp39*(-dtheta1*tmp3 + dtheta2*tmp22 + tau1*tmp2 + tmp12*tmp27*tmp34 - tmp2*tmp38)
+#    dfx[1, 0] = tmp21*(F1*tmp12 - tmp12*tmp9 - tmp14*tmp40)
+#    dfx[1, 1] = tmp21*(tmp12*tmp24 - tmp41)
+#    dfx[1, 2] = tmp21*(tmp12*tmp30 - tmp28*tmp40)
+#    dfx[1, 3] = tmp21*(-F1*dtheta1*tmp7 + tau1*tmp7 + tmp12*tmp36 + 2*tmp31 \
+#        + 2*tmp35 - tmp37*tmp42 - tmp38*tmp7) - tmp39*(F1*dtheta1*tmp12 \
+#            - dtheta2*tmp41 - tau1*tmp12 + tmp12*tmp38 - tmp34*tmp42)
+#    dfx[2, 0] = 1
+#    dfx[3, 1] = 1
+#    return dfx
+#
+#def jacobian_x_dot_wrt_u(theta2):
+#    dfu = np.zeros((4, 4))
+#    # Common Subexpressions:
+#    tmp0 = pow(R2, 2)
+#    tmp1 = M2*tmp0
+#    tmp2 = I2 + tmp1
+#    tmp3 = pow(L1, 2)
+#    tmp4 = M2*tmp3
+#    tmp5 = M1*pow(R1, 2)
+#    tmp6 = 1.0/(I1*I2 + I1*tmp1 + I2*tmp4 + I2*tmp5 \
+#        + pow(M2, 2)*tmp0*tmp3*pow(np.sin(theta2), 2) + tmp1*tmp5)
+#    tmp7 = L1*M2*R2*np.cos(theta2)
+#    tmp8 = tmp6*(-tmp2 - tmp7)
+#    # Jacobian Elements:
+#    dfu[0, 0] = tmp2*tmp6
+#    dfu[0, 1] = tmp8
+#    dfu[1, 0] = tmp8
+#    dfu[1, 1] = tmp6*(I1 + tmp2 + tmp4 + tmp5 + 2*tmp7)
+#    return dfu
 
-def jacobian_x_dot_wrt_u(theta2):
-    dfu = np.zeros((4, 4))
-    # Common Subexpressions:
-    tmp0 = pow(R2, 2)
-    tmp1 = M2*tmp0
-    tmp2 = I2 + tmp1
-    tmp3 = pow(L1, 2)
-    tmp4 = M2*tmp3
-    tmp5 = M1*pow(R1, 2)
-    tmp6 = 1.0/(I1*I2 + I1*tmp1 + I2*tmp4 + I2*tmp5 \
-        + pow(M2, 2)*tmp0*tmp3*pow(np.sin(theta2), 2) + tmp1*tmp5)
-    tmp7 = L1*M2*R2*np.cos(theta2)
-    tmp8 = tmp6*(-tmp2 - tmp7)
-    # Jacobian Elements:
-    dfu[0, 0] = tmp2*tmp6
-    dfu[0, 1] = tmp8
-    dfu[1, 0] = tmp8
-    dfu[1, 1] = tmp6*(I1 + tmp2 + tmp4 + tmp5 + 2*tmp7)
-    return dfu
+import numpy as np
+
+def compute_jacobian_simplified(x_new, u):
+    dtheta1 = x_new[0].item()
+    dtheta2 = x_new[1].item()
+    theta1 = x_new[2].item()
+    theta2 = x_new[3].item()
+    tau1 = u[0].item()
+    tau2 = u[1].item()
+    A = compute_jacobian_x(tau1, tau2, dtheta1, dtheta2, theta1, theta2)
+    B = compute_jacobian_u(theta2)
+    return A, B
+
+def compute_jacobian_x(tau1, tau2, dtheta1, dtheta2, theta1, theta2):
+    # Calcoli intermedi comuni
+    sin_theta2 = np.sin(theta2)
+    cos_theta2 = np.cos(theta2)
+    sin_theta1_theta2 = np.sin(theta1 + theta2)
+    cos_theta1_theta2 = np.cos(theta1 + theta2)
+
+    denom = (I1 * I2 + I1 * M2 * R2**2 + I2 * L1**2 * M2 + I2 * M1 * R1**2 +
+             L1**2 * M2**2 * R2**2 * sin_theta2**2 + M1 * M2 * R1**2 * R2**2)
+
+    # Matrice Jacobiana inizializzata a zero
+    jacobian_x_new_wrt_x = np.zeros((4, 4))
+
+    # Elemento [0,0]
+    jacobian_x_new_wrt_x[0, 0] = (dt * (-F1 * (I2 + M2 * R2**2) +
+                                        2 * L1 * M2 * R2 * dtheta1 * (I2 + L1 * M2 * R2 * cos_theta2 + M2 * R2**2) * sin_theta2 +
+                                        2 * L1 * M2 * R2 * dtheta2 * (I2 + M2 * R2**2) * sin_theta2) /
+                                  denom + 1)
+
+    # Elemento [0,1]
+    jacobian_x_new_wrt_x[0, 1] = (dt * (F2 * (I2 + L1 * M2 * R2 * cos_theta2 + M2 * R2**2) -
+                                        (I2 + M2 * R2**2) * (-L1 * M2 * R2 * dtheta2 * sin_theta2 -
+                                                             L1 * M2 * R2 * (2 * dtheta1 + dtheta2) * sin_theta2)) /
+                                  denom)
+
+    # Elemento [0,2]
+    jacobian_x_new_wrt_x[0, 2] = (dt * (G * M2 * R2 * (I2 + L1 * M2 * R2 * cos_theta2 + M2 * R2**2) * cos_theta1_theta2 -
+                                        (I2 + M2 * R2**2) * (G * M2 * R2 * cos_theta1_theta2 +
+                                                             G * (L1 * M2 + M1 * R1) * np.cos(theta1))) /
+                                  denom)
+
+    # Elemento [0,3]
+    jacobian_x_new_wrt_x[0, 3] = (-2 * L1**2 * M2**2 * R2**2 * dt * (
+        -F1 * dtheta1 * (I2 + M2 * R2**2) +
+        F2 * dtheta2 * (I2 + L1 * M2 * R2 * cos_theta2 + M2 * R2**2) +
+        M2 * R2 * (G * sin_theta1_theta2 + L1 * dtheta1**2 * sin_theta2) * (I2 + L1 * M2 * R2 * cos_theta2 + M2 * R2**2) +
+        tau1 * (I2 + M2 * R2**2) - tau2 * (I2 + L1 * M2 * R2 * cos_theta2 + M2 * R2**2) -
+        (I2 + M2 * R2**2) * (G * M2 * R2 * sin_theta1_theta2 + G * (L1 * M2 + M1 * R1) * np.sin(theta1) -
+                             L1 * M2 * R2 * dtheta2 * (2 * dtheta1 + dtheta2) * sin_theta2)) * sin_theta2 * cos_theta2 /
+        denom**2 +
+        dt * (-F2 * L1 * M2 * R2 * dtheta2 * sin_theta2 -
+              L1 * M2**2 * R2**2 * (G * sin_theta1_theta2 + L1 * dtheta1**2 * sin_theta2) * sin_theta2 +
+              L1 * M2 * R2 * tau2 * sin_theta2 +
+              M2 * R2 * (G * cos_theta1_theta2 + L1 * dtheta1**2 * cos_theta2) * (I2 + L1 * M2 * R2 * cos_theta2 + M2 * R2**2) -
+              (I2 + M2 * R2**2) * (G * M2 * R2 * cos_theta1_theta2 -
+                                   L1 * M2 * R2 * dtheta2 * (2 * dtheta1 + dtheta2) * cos_theta2)) /
+        denom)
+
+    # Elemento [1,0]
+    jacobian_x_new_wrt_x[1, 0] = (dt * (F1 * (I2 + L1 * M2 * R2 * cos_theta2 + M2 * R2**2) -
+                                        2 * L1 * M2 * R2 * dtheta1 * (I1 + I2 + L1**2 * M2 + 2 * L1 * M2 * R2 * cos_theta2 +
+                                                                      M1 * R1**2 + M2 * R2**2) * sin_theta2 -
+                                        2 * L1 * M2 * R2 * dtheta2 * (I2 + L1 * M2 * R2 * cos_theta2 + M2 * R2**2) * sin_theta2) /
+                                  denom)
+
+    # Elemento [1,1]
+    jacobian_x_new_wrt_x[1, 1] = (dt * (-F2 * (I1 + I2 + L1**2 * M2 + 2 * L1 * M2 * R2 * cos_theta2 + M1 * R1**2 + M2 * R2**2) +
+                                        (-L1 * M2 * R2 * dtheta2 * sin_theta2 -
+                                         L1 * M2 * R2 * (2 * dtheta1 + dtheta2) * sin_theta2) *
+                                        (I2 + L1 * M2 * R2 * cos_theta2 + M2 * R2**2)) /
+                                  denom + 1)
+
+    # Elemento [1,2]
+    jacobian_x_new_wrt_x[1, 2] = (dt * (-G * M2 * R2 * (I1 + I2 + L1**2 * M2 + 2 * L1 * M2 * R2 * cos_theta2 + M1 * R1**2 + M2 * R2**2) * cos_theta1_theta2 +
+                                        (G * M2 * R2 * cos_theta1_theta2 + G * (L1 * M2 + M1 * R1) * np.cos(theta1)) *
+                                        (I2 + L1 * M2 * R2 * cos_theta2 + M2 * R2**2)) /
+                                  denom)
+
+    # Elemento [1,3]
+    jacobian_x_new_wrt_x[1, 3] = (dt * (-F2 * L1 * M2 * R2 * dtheta2 * sin_theta2 -
+                                        L1 * M2**2 * R2**2 * (G * sin_theta1_theta2 + L1 * dtheta1**2 * sin_theta2) * sin_theta2 +
+                                        L1 * M2 * R2 * tau2 * sin_theta2 +
+                                        M2 * R2 * (G * cos_theta1_theta2 + L1 * dtheta1**2 * cos_theta2) * (I2 + L1 * M2 * R2 * cos_theta2 + M2 * R2**2) -
+                                        (I2 + M2 * R2**2) * (G * M2 * R2 * cos_theta1_theta2 -
+                                                             L1 * M2 * R2 * dtheta2 * (2 * dtheta1 + dtheta2) * cos_theta2)) /
+                                  denom)
+
+    # Elementi rimanenti
+    jacobian_x_new_wrt_x[2, 0] = dt
+    jacobian_x_new_wrt_x[2, 1] = 0
+    jacobian_x_new_wrt_x[2, 2] = 1
+    jacobian_x_new_wrt_x[2, 3] = 0
+    jacobian_x_new_wrt_x[3, 0] = 0
+    jacobian_x_new_wrt_x[3, 1] = dt
+    jacobian_x_new_wrt_x[3, 2] = 0
+    jacobian_x_new_wrt_x[3, 3] = 1
+
+    return jacobian_x_new_wrt_x
+
+def compute_jacobian_u(theta2):
+    # Calcoli intermedi comuni
+    sin_theta2 = np.sin(theta2)
+    cos_theta2 = np.cos(theta2)
+    pow_sin_theta2 = np.sin(theta2)**2
+    pow_cos_theta2 = np.cos(theta2)**2
+
+    denom = (I1 * I2 + I1 * M2 * R2**2 + I2 * L1**2 * M2 + I2 * M1 * R1**2 +
+             L1**2 * M2**2 * R2**2 * pow_sin_theta2 + M1 * M2 * R1**2 * R2**2)
+
+    # Matrice Jacobiana inizializzata a zero
+    jacobian_x_new_wrt_u = np.zeros((4, 4))
+
+    # Elemento [0,0]
+    jacobian_x_new_wrt_u[0, 0] = dt * (I2 + M2 * R2**2) / denom
+
+    # Elemento [0,1]
+    jacobian_x_new_wrt_u[0, 1] = dt * (-I2 - L1 * M2 * R2 * cos_theta2 - M2 * R2**2) / denom
+
+    # Elemento [1,0]
+    jacobian_x_new_wrt_u[1, 0] = dt * (-I2 - L1 * M2 * R2 * cos_theta2 - M2 * R2**2) / denom
+
+    # Elemento [1,1]
+    jacobian_x_new_wrt_u[1, 1] = dt * (I1 + I2 + L1**2 * M2 + 2 * L1 * M2 * R2 * cos_theta2 + M1 * R1**2 + M2 * R2**2) / denom
+
+    # Elementi rimanenti
+    jacobian_x_new_wrt_u[0, 2] = 0
+    jacobian_x_new_wrt_u[0, 3] = 0
+    jacobian_x_new_wrt_u[1, 2] = 0
+    jacobian_x_new_wrt_u[1, 3] = 0
+    jacobian_x_new_wrt_u[2, 0] = 0
+    jacobian_x_new_wrt_u[2, 1] = 0
+    jacobian_x_new_wrt_u[2, 2] = 0
+    jacobian_x_new_wrt_u[2, 3] = 0
+    jacobian_x_new_wrt_u[3, 0] = 0
+    jacobian_x_new_wrt_u[3, 1] = 0
+    jacobian_x_new_wrt_u[3, 2] = 0
+    jacobian_x_new_wrt_u[3, 3] = 0
+
+    return jacobian_x_new_wrt_u
+
+
 
 def hessian_x_dot_wrt_x(dtheta1, dtheta2, theta1, theta2, tau1):
     Hxx = np.zeros((4, 4, 4))
