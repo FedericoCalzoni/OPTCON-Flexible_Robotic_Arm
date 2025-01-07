71478b30518ccf6a51385445623bb98b12976c49
FedericoCalzoni <federicocalzoni01@gmail.com>
2025-01-07 10:38:23 +0100

    fix MPC

--- src/mpc.py
+++ src/mpc.py
@@ -4,7 +4,9 @@ from dynamics import dynamics, jacobian_x_new_wrt_x, jacobian_x_new_wrt_u
 import cost
 from parameters import T_pred, umax, umin
 
-def solver_linear_mpc(A, B, Q, R, x_t, t_start): 
+np.set_printoptions(linewidth=100)
+
+def solver_linear_mpc(A, B, Q, R, x_t, t_start, x_gen, u_gen): 
 
     x_size, u_size, T = B.shape
 
@@ -16,28 +18,46 @@ def solver_linear_mpc(A, B, Q, R, x_t, t_start):
     cost = 0
     constraints = []
 
-
     for tau in range(T_pred - 1):
         if t_start + tau >= T:
             A_tau = A[:,:,-1]
             B_tau = B[:,:,-1]
             Q_tau = Q[:,:,-1]
             R_tau = R[:,:,-1]
+            delta_x = x_mpc[:, tau] - x_gen[:,-1]
+            delta_u = u_mpc[:, tau] - u_gen[:,-1]
         else:
             A_tau = A[:,:,t_start+tau]
             B_tau = B[:,:,t_start+tau]
             Q_tau = Q[:,:,t_start+tau]
             R_tau = R[:,:,t_start+tau]
-                  
-        cost += cp.quad_form(x_mpc[:, tau] - x_mpc[:, tau], Q_tau) + cp.quad_form(u_mpc[:, tau] - u_mpc[:, tau], R_tau)
+            delta_x = x_mpc[:, tau] - x_gen[:,t_start+tau]
+            delta_u = u_mpc[:, tau] - u_gen[:,t_start+tau]
+            
+        
+        cost += cp.quad_form(delta_x, Q_tau) + cp.quad_form(delta_u, R_tau)
         constraints += [
             x_mpc[:, tau + 1] == A_tau @ x_mpc[:, tau] + B_tau @ u_mpc[:, tau],
             u_mpc[:, tau] <= umax,
             u_mpc[:, tau] >= umin,
+            
+            # # Angular velocity bounds (adjust these based on your system)
+            # x_mpc[0:2, tau] <= 1000,
+            # x_mpc[0:2, tau] >= -1000,
+            
+            # # Angle bounds (first two states)
+            # x_mpc[2:4, tau] <= 2*np.pi,
+            # x_mpc[2:4, tau] >= -2*np.pi,
         ]
 
+    
     # sums problem objectives and concatenates constraints
-    cost += cp.quad_form(x_mpc[:, T_pred - 1] - x_mpc[:, T_pred - 1], Q[:,:,-1])
+    
+    if t_start + T_pred - 1 >= T:
+        delta_xT = x_mpc[:, -1] - x_gen[:,-1]
+    else:
+        delta_xT = x_mpc[:, -1] - x_gen[:,t_start+T_pred-1]
+    cost += cp.quad_form(delta_xT, Q[:,:,-1])
     constraints += [x_mpc[:, 0] == x_t]
 
     # Solve the optimization problem
@@ -47,8 +67,8 @@ def solver_linear_mpc(A, B, Q, R, x_t, t_start):
     if problem.status == "infeasible":
     # Otherwise, problem.value is inf or -inf, respectively.
         print("Infeasible problem! CHECK YOUR CONSTRAINTS!!!")
-
-    return x_mpc.value, u_mpc.value
+    
+    return x_mpc.value, u_mpc.value, problem
 
 def compute_mpc(x_gen, u_gen):
 
@@ -78,10 +98,15 @@ def compute_mpc(x_gen, u_gen):
 
     for t in range(T-1):
         x_t_mpc = x_real_mpc[:,t]
-        x_mpc[:,:,t], u_mpc[:,:,t] = solver_linear_mpc(A, B, Q, R, x_t_mpc, t)
+        x_mpc[:,:,t], u_mpc[:,:,t], problem = solver_linear_mpc(A, B, Q, R, x_t_mpc, t, x_gen, u_gen)
         u_real_mpc[:,t] = u_mpc[:,0,t]
-    
         x_real_mpc[:,t+1] = dynamics(x_real_mpc[:,t], u_real_mpc[:,t])
+        
+        if t%100 == 0:
+            print("t = ", t)
+            print("Cost = ", problem.value)
+            print("x_mpc = ", x_mpc[:,:,t])
+            print("u_mpc = ", u_mpc[:,:,t])
 
     return x_real_mpc, u_real_mpc    
         
